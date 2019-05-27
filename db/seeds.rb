a598e9f33eb23c49756235c7aec4a162c000fe9c
Hector Hidalgo <marce.753@gmail.com>
2019-05-27 16:24:11 -0400

    Implemented authentication devise token gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Perrito.create!([
-  { nombre: Laika, edad:6, descripcion:'Es juguetona y tiene unos ojos radiantes.', raza:'Labrador', color:'Dorado'},
-  { nombre: Chiki, edad:2, descripcion:'Es un perro pequeño, tranquilo y ama usar soleras.', raza:'Labrador', color:'Marron'}
-])
+#Perrito.create!([
+ #{ nombre: Chiki, edad:2, descripcion:'Es un perro pequeño, tranquilo y ama usar soleras.', raza:'Labrador', color:'Marron'}
+#])
+User.create(email: 'user@example.com', nickname: 'UOne', name: 'User One', password: "monkey67")
